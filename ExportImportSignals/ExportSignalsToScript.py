dc0f741903ff402741c934d238c6007a9bf50e88
rainer@software-architects.at <4GO1dWmzP3izDvDArcq3>
2015-01-26 15:11:23 +0100

    Updated Signal Export Sample

--- ExportImportSignals/ExportSignalsToScript.py
+++ ExportImportSignals/ExportSignalsToScript.py
@@ -1,28 +1,93 @@
 # -*- coding: utf-8 -*-
+
 # =================================================================================================================
-#	SIGNAL DATA EXPORTER
-#	This script can be used to generate a Python script that re-creates the signals of the specified day.
+#	SIGNAL DATA EXPORT SCRIPT (see SampleExport.py for an example of how to use)
+# -----------------------------------------------------------------------------------------------------------------
+#	Copyright (c) 2015 software architects gmbh
+#	
+#	Permission is hereby granted, free of charge, to any person obtaining a copy
+#	of this software and associated documentation files (the "Software"), to deal
+#	in the Software without restriction, including without limitation the rights
+#	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#	copies of the Software, and to permit persons to whom the Software is
+#	furnished to do so, subject to the following conditions:
+#	
+#	The above copyright notice and this permission notice shall be included in
+#	all copies or substantial portions of the Software.
+#	
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#	THE SOFTWARE.
+# -----------------------------------------------------------------------------------------------------------------
+#	This script exports the activity log collected by time cockpit's signal tracker. The result is another
+#	script that can be used to recreate the activity log in a different time cockpit account.
+#	Note that this script has to have access to the decrypted activity log. Therefore, you can only export
+#	signals of the current user. Because of privacy restrictions, it is NOT POSSIBLE to export the
+#	activity log of a different user.
+#
+#	CAUTION: THE RESULTS MIGHT CONTAIN SENSITIVE DATA AS IT INCLUDES THE ENTIRE ACTIVITY LOG FOR
+#		THE SELECTED TIME PERIOD. THEREFORE, YOU HAVE TO PROPERLY PROTECT THE RESULTS.
 # =================================================================================================================
 
+# Import some .NET classes
 from System.Collections.Generic import Dictionary, List
 from System import String
 from System.Text import StringBuilder
 from System.IO import StreamWriter
-from System import Random
 clr.AddReference("System.Core")
 
-def extractTypeName(x):
-	helper = str(x)
-	helper = helper.Substring(0, helper.IndexOf(' '))
-	helper = helper.Substring(helper.LastIndexOf('.') + 1)
-	return helper
-	
-def exportSignals(dc, dayToExport, deviceNameToExport, targetFilePath, scenarioName, subroutineName, exportFileSystemSignals, exportWifiSignals):
+def exportSignalsToFile(dc,	dayToExport, deviceNameToExport, targetFilePath, subroutineName, \
+	exportFileSystemSignals = False, exportWifiSignals = False):
+	"""
+	Exports signal data of activity log to a file.
+
+	Keyword arguments:
+	dc -- DataContext that should be used to read activity log (TimeCockpit.Data.DataContext)
+	dayToExport -- Day that should be exported (DateTime; time-part has to be 0)
+	deviceNameToExport -- Name of the device that should be exported
+	targetFilePath -- Full path to the target file (UTF-8, overwrites file if it exists)
+	subroutineName -- Name of the generated routine (should be unique for each export)
+	exportFileSystemSignals -- Indicates whether file system signals should be exported (bool); default is False
+	exportWifiSignals -- Indicates whether WIFI signals should be exported (bool); default is False
+	"""
+	targetStream = StreamWriter(targetFilePath)
+	try:
+		exportSignalsToStream(dc, dayToExport, deviceNameToExport, targetStream, subroutineName, \
+			exportFileSystemSignals, exportWifiSignals)
+	finally:
+		targetStream.Close()
+
+def exportSignalsToStream(dc, dayToExport, deviceNameToExport, targetStream, subroutineName, \
+	exportFileSystemSignals, exportWifiSignals):
+	"""
+	Exports signal data of activity log to a stream.
+
+	Keyword arguments:
+	dc -- DataContext that should be used to read activity log (TimeCockpit.Data.DataContext)
+	dayToExport -- Day that should be exported (DateTime; time-part has to be 0)
+	deviceNameToExport -- Name of the device that should be exported
+	targetStream -- Target Stream (System.IO.StreamWriter)
+	subroutineName -- Name of the generated routine (should be unique for each export)
+	exportFileSystemSignals -- Indicates whether file system signals should be exported (bool); default is False
+	exportWifiSignals -- Indicates whether WIFI signals should be exported (bool); default is False
+	"""
+
+	# Internal helper function for extracting type name
+	def extractTypeName(x):
+		helper = str(x)
+		helper = helper.Substring(0, helper.IndexOf(' '))
+		helper = helper.Substring(helper.LastIndexOf('.') + 1)
+		return helper
+
 	# Specify all signal types that should be exported
 	signalTypes = [ "APP_CleansedChangeSetSignal", "APP_CleansedComputerActiveSignal", "APP_CleansedEmailSentSignal",
-		"APP_CleansedIpConnectSignal", "APP_CleansedPhoneCallSignal",
-		"APP_CleansedShortMessageSignal", "APP_CleansedUserActiveSignal", "APP_CleansedUserNoteSignal",
-		"APP_CleansedWindowActiveSignal", "APP_CleansedWorkItemChangeSignal" ]
+		"APP_CleansedIpConnectSignal", "APP_CleansedPhoneCallSignal", "APP_CleansedShortMessageSignal", 
+		"APP_CleansedUserActiveSignal", "APP_CleansedUserNoteSignal", "APP_CleansedWindowActiveSignal", 
+		"APP_CleansedWorkItemChangeSignal" ]
 	if exportFileSystemSignals:
 		# Only export file system signals if explicitly asked for
 		signalTypes.append("APP_CleansedFileSystemSignal")
@@ -30,8 +95,6 @@ def exportSignals(dc, dayToExport, deviceNameToExport, targetFilePath, scenarioN
 		# Only export WIFI signals if explicitly asked for
 		signalTypes.append("APP_CleansedWifiAvailableSignal")
 
-	writer = StreamWriter(targetFilePath)
-	rand = Random()
 	try:
 		chunks = dc.SelectWithParams({
 			"Query": "From C In SYS_Chunk.Include('SYS_Entity').Include('SYS_Device') " \
@@ -43,37 +106,38 @@ def exportSignals(dc, dayToExport, deviceNameToExport, targetFilePath, scenarioN
 		if (len(chunks) == 0):
 			raise Exception("No chunks found. Please review export filter criteria.")
 		
-		writer.WriteLine("# -*- coding: utf-8 -*-")
-		writer.WriteLine("# This is an auto-generated script")
-		writer.WriteLine("#\tScenario name: {0}", scenarioName)
-		writer.WriteLine("#\tGeneration date: {0}", DateTime.Today)
-		writer.WriteLine("#\tSource device name: {0}", deviceNameToExport)
-		writer.WriteLine("#\tSource day: {0}", dayToExport)
-		writer.WriteLine()
+		# Write documentation header
+		targetStream.WriteLine("# -*- coding: utf-8 -*-")
+		targetStream.WriteLine("# This is an auto-generated script")
+		targetStream.WriteLine("#\tGeneration date: {0}", DateTime.Today)
+		targetStream.WriteLine("#\tSource device name: {0}", deviceNameToExport)
+		targetStream.WriteLine("#\tSource day: {0}", dayToExport)
+		targetStream.WriteLine()
+
+		# Write necessary imports		
+		targetStream.WriteLine("from System.Collections.Generic import List")
+		targetStream.WriteLine()
 		
-		writer.WriteLine("from System.Collections.Generic import List")
-		writer.WriteLine("persist = False\t\t# Change to True to write changes to DB")
-		writer.WriteLine()
-		writer.WriteLine("def {0}(dc, targetDay):", subroutineName)
-			
-		writer.WriteLine("\tentities = dc.Select(\"From E In SYS_Entity Select E\")")
-		writer.WriteLine("\tdevice = dc.SelectSingle(\"From D In SYS_Device Select D\")")
-		writer.WriteLine("\tif device is None:")
-		writer.WriteLine("\t\traise Exception(\"No device found\")")
-		writer.WriteLine("\ttimeCorrection = targetDay - DateTime({0}, {1}, {2})", dayToExport.Year, dayToExport.Month, dayToExport.Day)
+		# Generate method that regenerates activity log
+		targetStream.WriteLine("def {0}(dc, targetDay):", subroutineName)
+		targetStream.WriteLine("\tentities = dc.Select(\"From E In SYS_Entity Select E\")")
+		targetStream.WriteLine("\tdevice = dc.SelectSingle(\"From D In SYS_Device Select D\")")
+		targetStream.WriteLine("\tif device is None:")
+		targetStream.WriteLine("\t\traise Exception(\"No device found\")")
+		targetStream.WriteLine("\ttimeCorrection = targetDay - DateTime({0}, {1}, {2})", dayToExport.Year, dayToExport.Month, dayToExport.Day)
 		
-		writer.WriteLine("\tdc.DbClient.BeginTransaction()")
-		writer.WriteLine("\ttry:")
+		targetStream.WriteLine("\tdc.DbClient.BeginTransaction()")
+		targetStream.WriteLine("\ttry:")
 		for chunk in chunks:
 			if (chunk.Entity.EntityName in signalTypes):
-				writer.WriteLine("\t\tchunk = dc.CreateChunk({{ \"BeginTime\": DateTime({0}, {1}, {2}, {3}, {4}, {5}) + timeCorrection,", chunk.BeginTime.Year, chunk.BeginTime.Month, chunk.BeginTime.Day, chunk.BeginTime.Hour, chunk.BeginTime.Minute, chunk.BeginTime.Second)
-				writer.WriteLine("\t\t\t\"EndTime\": DateTime({0}, {1}, {2}, {3}, {4}, {5}) + timeCorrection,", chunk.EndTime.Year, chunk.EndTime.Month, chunk.EndTime.Day, chunk.EndTime.Hour, chunk.EndTime.Minute, chunk.EndTime.Second)
-				writer.WriteLine("\t\t\t\"LogicalBeginTime\": DateTime({0}, {1}, {2}, {3}, {4}, {5}) + timeCorrection,", chunk.LogicalBeginTime.Year, chunk.LogicalBeginTime.Month, chunk.LogicalBeginTime.Day, chunk.LogicalBeginTime.Hour, chunk.LogicalBeginTime.Minute, chunk.LogicalBeginTime.Second)
-				writer.WriteLine("\t\t\t\"LogicalEndTime\": DateTime({0}, {1}, {2}, {3}, {4}, {5}) + timeCorrection,", chunk.LogicalEndTime.Year, chunk.LogicalEndTime.Month, chunk.LogicalEndTime.Day, chunk.LogicalEndTime.Hour, chunk.LogicalEndTime.Minute, chunk.LogicalEndTime.Second)
-				writer.WriteLine("\t\t\t\"Entity\": [e for e in entities if e.EntityName == \"{0}\"][0],", chunk.Entity.EntityName)
-				writer.WriteLine("\t\t\t\"Device\": device })")
+				targetStream.WriteLine("\t\tchunk = dc.CreateChunk({{ \"BeginTime\": DateTime({0}, {1}, {2}, {3}, {4}, {5}) + timeCorrection,", chunk.BeginTime.Year, chunk.BeginTime.Month, chunk.BeginTime.Day, chunk.BeginTime.Hour, chunk.BeginTime.Minute, chunk.BeginTime.Second)
+				targetStream.WriteLine("\t\t\t\"EndTime\": DateTime({0}, {1}, {2}, {3}, {4}, {5}) + timeCorrection,", chunk.EndTime.Year, chunk.EndTime.Month, chunk.EndTime.Day, chunk.EndTime.Hour, chunk.EndTime.Minute, chunk.EndTime.Second)
+				targetStream.WriteLine("\t\t\t\"LogicalBeginTime\": DateTime({0}, {1}, {2}, {3}, {4}, {5}) + timeCorrection,", chunk.LogicalBeginTime.Year, chunk.LogicalBeginTime.Month, chunk.LogicalBeginTime.Day, chunk.LogicalBeginTime.Hour, chunk.LogicalBeginTime.Minute, chunk.LogicalBeginTime.Second)
+				targetStream.WriteLine("\t\t\t\"LogicalEndTime\": DateTime({0}, {1}, {2}, {3}, {4}, {5}) + timeCorrection,", chunk.LogicalEndTime.Year, chunk.LogicalEndTime.Month, chunk.LogicalEndTime.Day, chunk.LogicalEndTime.Hour, chunk.LogicalEndTime.Minute, chunk.LogicalEndTime.Second)
+				targetStream.WriteLine("\t\t\t\"Entity\": [e for e in entities if e.EntityName == \"{0}\"][0],", chunk.Entity.EntityName)
+				targetStream.WriteLine("\t\t\t\"Device\": device })")
 				
-				writer.WriteLine("\t\tchunkContentList = List[EntityObject]()")
+				targetStream.WriteLine("\t\tchunkContentList = List[EntityObject]()")
 				for signal in chunk.Content:
 					signalContent = StringBuilder()
 					for prop in signal.Entity.Properties:
@@ -92,6 +156,8 @@ def exportSignals(dc, dayToExport, deviceNameToExport, targetFilePath, scenarioN
 								d = eval("signal.{0}".format(prop.Name))
 								signalContent.AppendFormat("{0}, {1}, {2}, {3}, {4}, {5}", d.Year, d.Month, d.Day, d.Hour, d.Minute, d.Second)
 								signalContent.Append(')')
+								if (prop.Name == "APP_BeginTime" or prop.Name == "APP_EndTime" or prop.Name == "APP_EventTime"):
+									signalContent.Append(" + timeCorrection")
 							elif (typeName == "BooleanProperty"):
 								b = eval("signal.{0}".format(prop.Name))
 								signalContent.Append("True" if b else "False")
@@ -104,19 +170,16 @@ def exportSignals(dc, dayToExport, deviceNameToExport, targetFilePath, scenarioN
 								signalContent.Append(n)
 							else:
 								raise Exception("MISSING TYPE {0}".format(typeName))
-					writer.WriteLine("\t\tchunkContentList.Add(dc.Create{0}({{ {1} }}))", chunk.Entity.EntityName, signalContent.ToString())
-				writer.WriteLine("\t\tchunk.Content = chunkContentList")
-				writer.WriteLine("\t\tdc.SaveObject(chunk)")
-		writer.WriteLine("\texcept:")
-		writer.WriteLine("\t\tif (dc.DbClient.TransactionCount > 0):")
-		writer.WriteLine("\t\t\tdc.DbClient.RollbackTransaction()")
-		writer.WriteLine("\t\traise")
-		writer.WriteLine("\tfinally:")
-		writer.WriteLine("\t\tif (dc.DbClient.TransactionCount > 0):")
-		writer.WriteLine("\t\t\tif(persist):")
-		writer.WriteLine("\t\t\t\tdc.DbClient.CommitTransaction()")
-		writer.WriteLine("\t\t\telse:")
-		writer.WriteLine("\t\t\t\tdc.DbClient.RollbackTransaction()")
+					targetStream.WriteLine("\t\tchunkContentList.Add(dc.Create{0}({{ {1} }}))", chunk.Entity.EntityName, signalContent.ToString())
+				targetStream.WriteLine("\t\tchunk.Content = chunkContentList")
+				targetStream.WriteLine("\t\tdc.SaveObject(chunk)")
+		targetStream.WriteLine("\texcept:")
+		targetStream.WriteLine("\t\tif (dc.DbClient.TransactionCount > 0):")
+		targetStream.WriteLine("\t\t\tdc.DbClient.RollbackTransaction()")
+		targetStream.WriteLine("\t\traise")
+		targetStream.WriteLine("\tfinally:")
+		targetStream.WriteLine("\t\tif (dc.DbClient.TransactionCount > 0):")
+		targetStream.WriteLine("\t\t\tdc.DbClient.CommitTransaction()")
 		
 	finally:
-		writer.Close()
+		targetStream.Close()
--- ExportImportSignals/SampleExport.py
+++ ExportImportSignals/SampleExport.py
@@ -1,10 +1,67 @@
 # -*- coding: utf-8 -*-
 
-# Script for generating demo data
+# =================================================================================================================
+#	SIGNAL DATA EXPORT SCRIPT
+# -----------------------------------------------------------------------------------------------------------------
+#	Copyright (c) 2015 software architects gmbh
+#	
+#	Permission is hereby granted, free of charge, to any person obtaining a copy
+#	of this software and associated documentation files (the "Software"), to deal
+#	in the Software without restriction, including without limitation the rights
+#	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#	copies of the Software, and to permit persons to whom the Software is
+#	furnished to do so, subject to the following conditions:
+#	
+#	The above copyright notice and this permission notice shall be included in
+#	all copies or substantial portions of the Software.
+#	
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#	THE SOFTWARE.
+# -----------------------------------------------------------------------------------------------------------------
+#	This script demonstrates how to use the export methods for activity logs (see ExportSignalsToScript.py
+#	for details).
+#
+#	CAUTION: THE RESULTING FILES MIGHT CONTAIN SENSITIVE DATA AS IT INCLUDES THE ENTIRE ACTIVITY LOG FOR
+#		THE SELECTED TIME PERIOD. THEREFORE, YOU HAVE TO PROPERLY PROTECT THE RESULTING FILE.
+# =================================================================================================================
+from System.IO import StringWriter
+
 dc = Context
 
+# Path where scripts are stored. You have to change that value according to your directory structure
 basePath = r"C:\Code\Github_TimeCockpit.Scripts\ExportImportSignals"
+
+# Execute ExportSignalsToScript.py so that we get the necessary export functions
+# exportSignalsToFile and exportSignalsToStream
 execfile(basePath + r"\ExportSignalsToScript.py")
 
-exportSignals(Context, DateTime(2015, 1, 26), "SIERRA3", basePath + "\\SampleScenario.py", "SampleScenario", "SampleScenario", True, True)
+# Export signals of a specific date to a file
+exportSignalsToFile(Context, DateTime(2015, 1, 26)	# Date to export
+	, "SIERRA3"										# Name of the device to export \
+	, basePath + "\\TypicalOfficeDayLog.g.py"		# Name of target file \
+	, "GenerateTypicalOfficeDayLog"					# Name of the generated method \
+	, False, False)									# No need to generate file and WIFI signals
+
+# Export multiple days (January 20th to 23th) in a loop
+for day in range(20, 23 + 1):
+	exportSignalsToFile(Context, DateTime(2015, 1, day), "SIERRA3"
+		, basePath + "\\Signals{0}.g.py".format(day)	# Name of target file \
+		, "GenerateSignals{0}".format(day)				# Name of the generated method \
+		, False, False)									# No need to generate file and WIFI signals
+
+# Export signals to a string
+writer = StringWriter()
+exportSignalsToStream(Context, DateTime(2015, 1, 25)	# Date to export
+	, "SIERRA3"											# Name of the device to export \
+	, writer											# Target stream (backed by StringBuilder) \
+	, "GenerateTypicalDevelopmentDayLog"				# Name of the generated method \
+	, False, False)										# No need to generate file and WIFI signals
+# Print source of generated method
+print writer.ToString().Substring(50) + " ..."
+
 print "Done!"
